fb617179db2ba60bceb5530a5e94fdf6464a08eb
laluridhoo <ridhobingkir@gmail.com>
2025-11-28 23:49:46 +0800

    Menyelesaikan fase 2 manajemen data game

--- src/api/services/game.service.ts
+++ src/api/services/game.service.ts
@@ -1,11 +1,8 @@
-import type { Game } from "../../models/types";
 import { GameRepository } from "../repositories/game.repository";
 
 export const GameService = {
-  async getGames(): Promise<Game[]> {
+  async getGames() {
     // Temp: tidak ada bisnis logic rumit, hanya ambil semua game.
     return GameRepository.findAll();
   },
 };
-
-
